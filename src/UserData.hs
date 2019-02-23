f87571848c011187119557bee7224980b3f435de
whybin <31753349+whybin@users.noreply.github.com>
2019-02-22 19:41:44 -0500

    Add decoding for user data

--- src/UserData.hs
+++ src/UserData.hs
@@ -1,10 +1,15 @@
-module UserData where
+module UserData
+    ( dataFromRaw
+    , rawFromData
+    ) where
 
 import Data.Aeson ((.=))
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy as B (ByteString)
-import qualified Data.Text as T (pack)
-import qualified Data.Time.Calendar as TC (Day)
+import qualified Data.HashMap.Lazy as H (toList)
+import qualified Data.Text as T (pack, unpack)
+import qualified Data.Text.Read as T (decimal)
+import qualified Data.Time.Calendar as TC (Day(..))
 
 import qualified Tag as Tg (RawTag)
 
@@ -19,5 +24,17 @@ instance A.ToJSON AllData where
     toJSON = A.object . map kvFromDaily . getAllData
     toEncoding = A.pairs . mconcat . map kvFromDaily . getAllData
 
+instance A.FromJSON AllData where
+    parseJSON = A.withObject "AllData" $
+        fmap AllData . traverse toDaily . H.toList
+            where toDaily (dayStr, dataStr) =
+                case T.decimal dayStr of
+                  Left _ -> fail $ "Failed to parse day: " ++ T.unpack dayStr
+                  Right (day, _) -> sequenceA ( TC.ModifiedJulianDay day
+                                              , A.parseJSON dataStr
+                                              )
 rawFromData :: AllData -> B.ByteString
 rawFromData = A.encode
+
+dataFromRaw :: B.ByteString -> Maybe AllData
+dataFromRaw = A.decode
