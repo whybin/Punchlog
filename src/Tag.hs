7029b8781f015019bfd62d14f996cc27d47dfac0
whybin <31753349+whybin@users.noreply.github.com>
2019-02-24 12:08:11 -0500

    Move tag data to Tag module

--- src/Tag.hs
+++ src/Tag.hs
@@ -1,9 +1,17 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Tag where
+module Tag
+    ( AllTags
+    ) where
 
+import Data.Aeson ((.=))
+import qualified Data.Aeson as A
 import qualified Data.Aeson.TH as A (defaultOptions, deriveJSON)
-import qualified Data.Text as T (Text)
+import qualified Data.ByteString.Lazy as B (ByteString)
+import qualified Data.HashMap.Lazy as H (toList)
+import qualified Data.Text as T (Text, pack, unpack)
+import qualified Data.Text.Read as T (decimal)
+import qualified Data.Time.Calendar as TC (Day(..))
 
 import qualified TimeUnit as TU
 
@@ -14,5 +22,31 @@ data RawTag = RawTag { tagId      :: Integer
                      }
 $(A.deriveJSON A.defaultOptions ''RawTag)
 
-newtype UiTag = UiTag { raw :: RawTag }
+-- Aggregate tag data
 
+type DayTags = (TC.Day, [RawTag])
+
+kvFromDaily :: A.KeyValue kv => DayTags -> kv
+kvFromDaily (day, tags) = (T.pack . show $ day) .= tags
+
+newtype AllTags = AllTags { getAllTags :: [DayTags] }
+
+instance A.ToJSON AllTags where
+    toJSON = A.object . map kvFromDaily . getAllTags
+    toEncoding = A.pairs . mconcat . map kvFromDaily . getAllTags
+
+instance A.FromJSON AllTags where
+    parseJSON = A.withObject "AllTags" $
+        fmap AllTags . traverse toDaily . H.toList
+            where toDaily (dayStr, dataStr) =
+                    case T.decimal dayStr of
+                      Left _ ->
+                          fail $ "Failed to parse day: " ++ T.unpack dayStr
+                      Right (day, _) ->
+                          sequenceA ( TC.ModifiedJulianDay day
+                                    , A.parseJSON dataStr
+                                    )
+
+-- UI tags
+
+newtype UiTag = UiTag { raw :: RawTag }
--- src/UserData.hs
+++ src/UserData.hs
@@ -1,40 +1,6 @@
-module UserData
-    ( dataFromRaw
-    , rawFromData
-    ) where
+module UserData where
 
-import Data.Aeson ((.=))
-import qualified Data.Aeson as A
-import qualified Data.ByteString.Lazy as B (ByteString)
-import qualified Data.HashMap.Lazy as H (toList)
-import qualified Data.Text as T (pack, unpack)
-import qualified Data.Text.Read as T (decimal)
-import qualified Data.Time.Calendar as TC (Day(..))
+import qualified Tag (AllTags)
 
-import qualified Tag as Tg (RawTag)
-
-type DailyData = (TC.Day, [Tg.RawTag])
-
-kvFromDaily :: A.KeyValue kv => DailyData -> kv
-kvFromDaily (day, tags) = (T.pack . show $ day) .= tags
-
-newtype AllData = AllData { getAllData :: [DailyData] }
-
-instance A.ToJSON AllData where
-    toJSON = A.object . map kvFromDaily . getAllData
-    toEncoding = A.pairs . mconcat . map kvFromDaily . getAllData
-
-instance A.FromJSON AllData where
-    parseJSON = A.withObject "AllData" $
-        fmap AllData . traverse toDaily . H.toList
-            where toDaily (dayStr, dataStr) =
-                case T.decimal dayStr of
-                  Left _ -> fail $ "Failed to parse day: " ++ T.unpack dayStr
-                  Right (day, _) -> sequenceA ( TC.ModifiedJulianDay day
-                                              , A.parseJSON dataStr
-                                              )
-rawFromData :: AllData -> B.ByteString
-rawFromData = A.encode
-
-dataFromRaw :: B.ByteString -> Maybe AllData
-dataFromRaw = A.decode
+newtype UserData = UserData { tags :: Tag.AllTags
+                            }
