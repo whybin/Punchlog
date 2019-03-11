82d71c0088bebbb095be10d1f180a725cbaf0971
whybin <31753349+whybin@users.noreply.github.com>
2019-03-11 11:56:37 -0400

    Add util function

--- src/Util.hs
+++ src/Util.hs
@@ -1,6 +1,13 @@
-module Util where
+module Util
+    ( liftReader
+    , untransformReader
+    ) where
 
-import Control.Monad.Reader (Reader, ReaderT(..), runReader)
+import Control.Monad.Reader (Reader, ReaderT(..), mapReaderT, runReader)
+import Data.Functor.Identity (Identity(..))
 
 liftReader :: Monad m => Reader r a -> ReaderT r m a
 liftReader reader = ReaderT $ return . runReader reader
+
+untransformReader :: Monad m => ReaderT r m a -> Reader r (m a)
+untransformReader = mapReaderT Identity
