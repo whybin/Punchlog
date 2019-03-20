863169410d5eaf0d6104a17b43b07be47ef446c7
whybin <31753349+whybin@users.noreply.github.com>
2019-03-20 14:56:13 -0400

    Move module for tags

---
+++ src/Tag/Base.hs
@@ -0,0 +1,49 @@
+module Tag.Base
+    ( newTag
+    , addTag
+    , tagsOnDay
+    ) where
+
+import qualified Data.Foldable as F (find)
+
+import qualified Data.Text as T (Text)
+import qualified Data.Time.Calendar as TC (Day)
+
+import Tag.Class (AllTags(..), DayTags(..), RawTag(..))
+import qualified TimeUnit as TU (TimestampUtc(..))
+
+newTag :: TU.TimestampUtc -> T.Text -> RawTag
+newTag timestamp text' =
+    RawTag { tagId = 0
+           , text = text'
+           , categoryId = 0
+           , time = timestamp
+           }
+
+singletonDayTags :: RawTag -> DayTags
+singletonDayTags tag =
+    let day = TU.dayUtc $ time tag
+     in DayTags (day, [tag])
+
+addTag :: RawTag -> AllTags -> AllTags
+addTag tag (AllTags all) = AllTags . insertByDay all $ singletonDayTags tag
+
+insertByDay :: [DayTags] -> DayTags -> [DayTags]
+insertByDay [] d = [d]
+insertByDay ds d@(DayTags (day, tag)) =
+    let (after, before) = span (> d) ds
+        before' =
+            case before of
+              [] -> [d]
+              b:bs | d < b -> d:b:bs
+              b@(DayTags (_, tag')):bs | d == b ->
+                  DayTags (day, tag ++ tag') : bs
+     in after ++ before'
+
+tagsOnDay :: AllTags -> TC.Day -> [RawTag]
+tagsOnDay (AllTags allTags) day = maybe [] getTags $ F.find onGivenDay allTags
+  where
+    onGivenDay :: DayTags -> Bool
+    onGivenDay (DayTags (day', _)) = day' == day
+    getTags :: DayTags -> [RawTag]
+    getTags (DayTags (_, tags)) = tags
